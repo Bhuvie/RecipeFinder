3624498ae8083cc20f554fa2ea7f5b5157144fd5
Bhuvanesh Rajakarthikeyan <rbhuvanesh.legends@gmail.com>
2017-11-23 23:23:52 -0600

    Nov 23 i.2

---
+++ app/src/main/java/com/bhuvie/recipefinder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bhuvie.recipefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
